e1b57acd59be87c05ec9b9af4d1b71dab51b788f
enkhara <50669024+enkhara@users.noreply.github.com>
2020-03-07 18:03:30 +0100

    geoJson

--- graphic.js
+++ graphic.js
@@ -1,6 +1,117 @@
-console.log('hola desde graphics');
 
 
-const svgGraph=d3.select('#graphic')
-.append('svg');
+ const width = 500;
+ const height = 500;
 
+ var svgGraph = d3.select('#graphic')
+    .append('svg');
+
+svgGraph   
+    .attr('width', width)
+    .attr('height', height)
+
+
+function drawGraph(name, bedrooms, controlVar){
+    if (controlVar>1){
+        svgGraph.remove('svg')
+        svgGraph = d3.select('#graphic')
+        .append('svg');
+    
+        svgGraph   
+            .attr('width', width)
+            .attr('height', height)
+    }
+
+   /*
+    window.onmousemove = function (){
+        var x = window.event.clientX;
+        var y = window.event.clientY;
+        console.log(x, y)
+    }*/
+    
+    const rectWidth = 70;
+    let maxInput = 0;
+    
+    for (let i = 0; i<bedrooms.length; i++) {
+        if (maxInput < bedrooms[i].total) {
+            maxInput = bedrooms[i].total;            
+        }
+    } 
+
+    const scaleY = d3   .scaleLinear()
+                        .domain([0, maxInput])
+                        .range([height -40, 40])
+                        .nice();
+        
+    const scaleX = d3.scaleLinear()
+                    .domain([0, 5])
+                     .range([20, width - 20])
+        
+                    
+    const graphicGroup = svgGraph.selectAll('g')
+                        .data(bedrooms)
+                        .enter()
+                        .append('g')
+
+
+    const rect = graphicGroup.append('rect');
+
+    rect
+        .attr('x', posX)
+        .attr('y', posY)
+        .attr('width', rectWidth)
+        .attr('height', scale)
+    
+    //Axis
+    
+
+    const axisX = d3.axisBottom(scaleX).ticks(0);
+
+    graphicGroup.append('g')
+                .call(axisX)
+                .attr('transform', `translate(0, ${height-40})`);
+
+
+    const axisY = d3.axisLeft(scaleY).ticks(maxInput)
+    
+    graphicGroup.append('g')
+                .call(axisY)
+                .attr('transform', `translate(${width-(width-20)},0)`)
+
+    //Axis END
+
+    function scale(d) {
+        const scaleNum = (height - 80) / maxInput;
+        if (d.total == 0){
+            return 1
+        }else{
+            return scaleNum * d.total;
+        }
+        
+    }
+        
+    function posX ( d, index )  {
+        return index * ( rectWidth + 20 ) +35;
+    }
+    
+    function posY (d) {
+        return height - 40 - scale(d);
+    }
+
+    const text = graphicGroup.append('text');
+
+    text
+        .attr('x', posX)
+        .attr('y', height-20)  
+        .text(d => {
+            return `${d.bedrooms} bedrooms`   
+        });
+    const title = graphicGroup.append('text');
+    
+    title 
+        .attr('x', 35)
+        .attr('y', 20)
+        .text(name)
+        
+
+}
--- map.js
+++ map.js
@@ -1,10 +1,19 @@
 console.log('hola desde map');
 
-d3.json('https://gist.githubusercontent.com/miguepiscy/2d431ec3bc101ef62ff8ddd0e476177f/raw/2482274db871e60195b7196c602700226bdd3a44/practica.json')
+/*d3.json('https://gist.githubusercontent.com/miguepiscy/2d431ec3bc101ef62ff8ddd0e476177f/raw/2482274db871e60195b7196c602700226bdd3a44/practica.json')
     .then((featureCollection) => {
         console.log(featureCollection);
         drawMap(featureCollection);
          
+    })*/
+
+d3.json('https://storage.cloud.google.com/enkhara/geoJson/airbnb-listings.geojson')
+    .then((featureCollection) => {
+        console.log(featureCollection)
+        drawMap(featureCollection)
+    })
+    .catch((error) => {
+        console.log('error', error);
     });
 
 
@@ -19,56 +28,67 @@ d3.json('https://gist.githubusercontent.com/miguepiscy/2d431ec3bc101ef62ff8ddd0e
 
         svgMap
             .attr('width', width)
-            .attr('height', height);
+            .attr('height', height)
+            
             
         const projection = d3.geoMercator()
-            .fitSize([width - border, height - border], featureCollection)
-            .center(center)
-            .translate([width/2, height/2]);
+                            .fitSize([width - border, height - border], featureCollection)
+                            .center(center)
+                            .translate([width/2, height/2]);
         
         const pathProjection = d3.geoPath().projection(projection);
         const features = featureCollection.features;
-        console.log(features);
         const groupMap = svgMap.append('g').attr('class', 'map');
 
-        
         const neighborhood = groupMap.selectAll('.neighborhood')
-            .data(features)
-            .enter()
-            .append('path');
-
+                                    .data(features)
+                                    .enter()
+                                    .append('path');
+        
         neighborhood.attr('d', (d) => {
             d.opacity=1;
-            return pathProjection(d);
+            return pathProjection(d)
         });
+ 
+
+        //Mouse EVENTS
+        groupMap.selectAll('path')
+                .on('mouseover', function(){
+                    d3.select(this).style('fill-opacity', 1.0);
+                })
+
+        groupMap.selectAll('path')
+                .on('mouseout', function(){
+                    d3.select(this).style('fill-opacity', 0.7);
+                })
+
+        let clicks = 0;
 
         neighborhood.on('click', function clickNeighborhood(d)  {
-            d.opacity = d.opacity ? 0 : 1;
-            d3.select(this).attr('opacity', d.opacity);
-            console.log(d);
-            console.log('barrio', d.properties.name);
-            console.log('precio medio', d.properties.avgprice);
-            console.log('habitaciones', d.properties.avgbedrooms);   
-           
+            clicks ++
+            
+            drawGraph(d.properties.name, d.properties.avgbedrooms, clicks)
         })
+        //Mouse EVENT END
         
         //legend
         const xMax = d3.max(features, (d) => d.properties.avgprice);
         const xMin = d3.min(features, (d) => d.properties.avgprice);
         
         const colorScale = d3.scaleLinear()
-            .domain([xMin, xMax])
-            .range(['yellow', 'red']);
-        const legend = d3.legendColor()
-            .scale(colorScale);
+                            .domain([xMin, xMax])
+                            .range(['yellow', 'red']);
+        const legend = d3   .legendColor()
+                            .scale(colorScale);
         
-        svgMap.append('g')
-            .call(legend)
-            .attr('transform', 'translate(400,10)')
+        svgMap  .append('g')
+                .call(legend)
+                .attr('transform', 'translate(400,10)')
         
         neighborhood.attr('fill',(d) => colorScale(d.properties.avgprice))
-        console.log(xMax,xMin)
-        
+                    .attr('fill-opacity', 0.7)
+    
+        //legend End
     }
 
    
